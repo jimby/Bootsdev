e2bbe5eb0405495826fddcb61af588de36ef36d1
Jim <jsdalton@cox.net>
2015-12-28 06:14:11 -0800

    Initial commit

---
+++ Includes/gimee.php
@@ -0,0 +1,5 @@
+<?php
+
+$wid= getWID.php;
+        
+
---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>menu.php</title>
+    </head>
+    <body>
+        <a href="customer/findCustomer.php">Customers</a><br> 
+		<a href="WarehouseInventory/editWarehouseInventory.php">warehouse,inventory</a><br>
+		<a href="WarehouseInventory/find_file_pix.php"> find products</a><br>
+        <a href="users/users.php">Users menu</a><br>
+        <a href="orders/Orders.php">Orders menu</a><br>
+        <a href="index.php">return</a><br>
+  	
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
